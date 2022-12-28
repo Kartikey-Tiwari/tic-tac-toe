85109ebc13b76722dc5dc406c0e329e604596f99
kt <98679116+Kartikey-Tiwari@users.noreply.github.com>
2022-12-28 12:43:17 +0530

    refactor: separate modules gameboard, gamecontroller, displaycontroller

--- script.js
+++ script.js
@@ -1,139 +1,158 @@
-(() => {
-  const activePlayerDOM = document.querySelector(".activePlayer");
-  const info = document.querySelector(".game-info");
+const GameBoard = (() => {
+  const board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  let cellsFilled = 0;
 
-  function playerFactory(name, symbol) {
-    return {
-      name,
-      symbol,
-    };
+  function checkWinner() {
+    for (let i = 0; i < 3; i++) {
+      if (
+        board[i][0] !== "" &&
+        board[i][0] == board[i][1] &&
+        board[i][1] == board[i][2]
+      )
+        return true;
+    }
+
+    for (let i = 0; i < 3; i++) {
+      if (
+        board[0][i] !== "" &&
+        board[0][i] == board[1][i] &&
+        board[1][i] == board[2][i]
+      )
+        return true;
+    }
+
+    if (
+      board[0][0] !== "" &&
+      board[0][0] == board[1][1] &&
+      board[1][1] == board[2][2]
+    )
+      return true;
+    else if (
+      board[0][2] !== "" &&
+      board[0][2] == board[1][1] &&
+      board[1][1] == board[2][0]
+    )
+      return true;
+    else return false;
   }
 
+  function move(row, col, mark) {
+    board[row][col] = mark;
+    cellsFilled++;
+  }
+
+  function reset() {
+    cellsFilled = 0;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        board[i][j] = "";
+      }
+    }
+  }
+
+  function getCellsFilled() {
+    return cellsFilled;
+  }
+
+  return {
+    move,
+    checkWinner,
+    reset,
+    getCellsFilled,
+  };
+})();
+
+function playerFactory(name, mark) {
+  function getName() {
+    return name;
+  }
+  function getMark() {
+    return mark;
+  }
+  return {
+    getName,
+    getMark,
+  };
+}
+
+const GameController = (() => {
+  let isGameOver = false;
   const player1 = playerFactory("Player 1", "X");
   const player2 = playerFactory("Player 2", "O");
   let activePlayer = player1;
 
   function switchPlayers() {
     activePlayer = activePlayer === player1 ? player2 : player1;
+    DisplayController.displayMessage(`${activePlayer.getName()}'s turn`);
   }
 
-  const boardElement = document.querySelector(".board");
-  const gameboard = (() => {
-    let cellsFilled = 0;
-    const board = [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""],
-    ];
-
-    function checkWinner() {
-      for (let i = 0; i < 3; i++) {
-        if (
-          board[i][0] !== "" &&
-          board[i][0] == board[i][1] &&
-          board[i][1] == board[i][2]
-        )
-          return true;
-      }
-
-      for (let i = 0; i < 3; i++) {
-        if (
-          board[0][i] !== "" &&
-          board[0][i] == board[1][i] &&
-          board[1][i] == board[2][i]
-        )
-          return true;
-      }
-
-      if (
-        board[0][0] !== "" &&
-        board[0][0] == board[1][1] &&
-        board[1][1] == board[2][2]
-      )
-        return true;
-      else if (
-        board[0][2] !== "" &&
-        board[0][2] == board[1][1] &&
-        board[1][1] == board[2][0]
-      )
-        return true;
-      else return false;
-    }
-
-    function move(row, col, mark) {
-      if (board[row][col] === "") {
-        board[row][col] = mark;
-        cellsFilled++;
-        return true;
-      } else {
-        return false;
-      }
-    }
-
-    function reset() {
-      keepPlaying = true;
-      activePlayer = player1;
-      cellsFilled = 0;
-      activePlayerDOM.textContent = activePlayer.name;
-      info.textContent = "'s turn";
-      for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-          board[i][j] = "";
-        }
-      }
-      for (const el of boardElement.children) {
-        el.textContent = "";
-      }
-    }
-
-    function getCellsFilled() {
-      return cellsFilled;
-    }
-
-    return {
-      move,
-      checkWinner,
-      reset,
-      getCellsFilled,
-    };
-  })();
-
-  let keepPlaying = true;
-
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      const cell = document.createElement("div");
-      cell.addEventListener("click", () => {
-        if (!keepPlaying || gameboard.getCellsFilled() === 9) return;
-        const moved = gameboard.move(i, j, activePlayer.symbol);
-        if (moved) {
-          cell.textContent = activePlayer.symbol;
-          if (gameboard.checkWinner()) {
-            keepPlaying = false;
-            info.textContent = ` has won the game!`;
-          } else if (gameboard.getCellsFilled() === 9) {
-            info.textContent = "It's a tie!";
-            activePlayerDOM.textContent = "";
-          } else {
-            switchPlayers();
-            activePlayerDOM.textContent = activePlayer.name;
-          }
-        }
-      });
-
-      cell.classList.add("cell");
-      if (i !== 0) cell.style.borderTop = "2px solid black";
-      else cell.style.borderTop = "3px solid grey";
-      if (j !== 0) cell.style.borderLeft = "2px solid black";
-      else cell.style.borderLeft = "3px solid grey";
-      if (i !== 2) cell.style.borderBottom = "2px solid black";
-      else cell.style.borderBottom = "3px solid grey";
-      if (j !== 2) cell.style.borderRight = "2px solid black";
-      else cell.style.borderRight = "3px solid grey";
-      boardElement.appendChild(cell);
+  function makeMove(i, j, cell) {
+    GameBoard.move(i, j, activePlayer.getMark());
+    DisplayController.setMark(cell, activePlayer.getMark());
+    if (GameBoard.checkWinner()) {
+      DisplayController.displayMessage(`${activePlayer.getName()} wins!`);
+      isGameOver = true;
+    } else if (GameBoard.getCellsFilled() === 9) {
+      DisplayController.displayMessage(`It's a tie!`);
+      isGameOver = true;
+    } else {
+      switchPlayers();
     }
   }
 
+  function isOver() {
+    return isGameOver;
+  }
+
+  function reset() {
+    activePlayer = player1;
+    isGameOver = false;
+    DisplayController.displayMessage(`${activePlayer.getName()}'s turn`);
+  }
+
+  return {
+    makeMove,
+    isOver,
+    reset,
+  };
+})();
+
+const DisplayController = (() => {
+  const gameInfo = document.querySelector(".game-info");
+  const boardCells = document.querySelectorAll(".cell");
   const restartBtn = document.querySelector(".restart");
-  restartBtn.addEventListener("click", gameboard.reset);
+
+  boardCells.forEach((cell) => {
+    cell.addEventListener("click", () => {
+      if (GameController.isOver() || cell.textContent !== "") return;
+      const row = +cell.dataset.row;
+      const col = +cell.dataset.col;
+      GameController.makeMove(row, col, cell);
+    });
+  });
+
+  restartBtn.addEventListener("click", () => {
+    GameBoard.reset();
+    GameController.reset();
+    boardCells.forEach((cell) => {
+      cell.textContent = "";
+    });
+  });
+
+  function displayMessage(message) {
+    gameInfo.textContent = message;
+  }
+
+  function setMark(cell, mark) {
+    cell.textContent = mark;
+  }
+
+  return {
+    displayMessage,
+    setMark,
+  };
 })();
